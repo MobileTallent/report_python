b3683e1ba9f040f73ce1ec4763a3d262b965e4f7
mobiletallent <mobiletallent@outlook.com>
2023-04-19 11:32:16 +0300

    update(4.19)

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def index():
 
         tracks = navixy_client.get_all_tracks()
     
-        return render_template('index.html', tracks=tracks)
+        return render_template('index.html', tracks=tracks, session_key=session_key)
     else:
         return render_template('error.html')
 
@@ -25,55 +25,78 @@ def header1():
     button_clicked = request.form['time_period']
     track_id = request.form['track_id']
     track_label = request.form['track_label']
+    session_key = request.form['session_key']
     now = datetime.datetime.now()
 
     if button_clicked == '24_hours':
         start_date = now - datetime.timedelta(days=1).strftime("%Y-%m-%d 00:00:00")
         end_date = now.strftime("%Y-%m-%d 23:59:59")
+
+        tracks = return_track_array(track_id, track_label, start_date, end_date, session_key)
     elif button_clicked == '1_week':
         start_date = now - datetime.timedelta(weeks=1).strftime("%Y-%m-%d 00:00:00")
         end_date = now.strftime("%Y-%m-%d 23:59:59")
-    else:
-        # Custom time period selected
-        start_date = datetime.datetime.strptime(request.form['start_date'], '%Y-%m-%d')
-        end_date = datetime.datetime.strptime(request.form['end_date'], '%Y-%m-%d')
-    
-    tracks = return_track_array(track_id, track_label, start_date, end_date)
 
+        tracks = return_track_array(track_id, track_label, start_date, end_date, session_key)
+    else:
+
+        if request.form['start_date'] and request.form['end_date']:
+            # Custom time period selected
+            start_date = datetime.datetime.strptime(request.form['start_date'], '%Y-%m-%d')
+            end_date = datetime.datetime.strptime(request.form['end_date'], '%Y-%m-%d')
+
+            tracks = return_track_array(track_id, track_label, start_date, end_date, session_key)
+        else:
+            tracks = []
+    
     if len(tracks) > 0:
         save_file = filemaker.export_data1(tracks)
         return send_file(save_file, as_attachment=True)
     else:
         print("There is no raw data to be exported")
-        return redirect(url_for('index'))
+        return redirect(url_for('index', session_key=session_key))
 
 @app.route('/header2', methods=['POST'])
 def header2():
-    start_date = datetime.datetime.strptime(request.form['start_date'], '%Y-%m-%d')
-    end_date = datetime.datetime.strptime(request.form['end_date'], '%Y-%m-%d')
-    track_id = request.form['track_id']
-    track_label = request.form['track_label']
 
-    tracks = return_track_array(track_id, track_label, start_date, end_date)
-
-    save_file = filemaker.export_data2(tracks, track_label, start_date, end_date)
+    session_key = request.form['session_key']
     
-    return send_file(save_file, as_attachment=True)
+    if request.form['start_date'] and request.form['end_date']:
+
+        start_date = datetime.datetime.strptime(request.form['start_date'], '%Y-%m-%d')
+        end_date = datetime.datetime.strptime(request.form['end_date'], '%Y-%m-%d')
+        track_id = request.form['track_id']
+        track_label = request.form['track_label']
+        
+
+        tracks = return_track_array(track_id, track_label, start_date, end_date, session_key)
+
+        save_file = filemaker.export_data2(tracks, track_label, start_date, end_date)
+        
+        return send_file(save_file, as_attachment=True)
+    else:
+        return redirect(url_for('index', session_key=session_key))
 
 
 @app.route('/header3', methods=['POST'])
 def header3():
-    track_id = request.form['track_id']
-    track_label = request.form['track_label']
-    start_date = datetime.datetime.strptime(request.form['start_date'], '%Y-%m-%d')
-    end_date = datetime.datetime.strptime(request.form['end_date'], '%Y-%m-%d')
 
-    tracks = return_track_array(track_id, track_label, start_date, end_date)
+    session_key = request.form['session_key']
+    
+    if request.form['start_date'] and request.form['end_date']:
 
-    save_file = filemaker.export_data3(tracks)
+        track_id = request.form['track_id']
+        track_label = request.form['track_label']
+        start_date = datetime.datetime.strptime(request.form['start_date'], '%Y-%m-%d')
+        end_date = datetime.datetime.strptime(request.form['end_date'], '%Y-%m-%d')
 
-    return send_file(save_file, as_attachment=True)
+        tracks = return_track_array(track_id, track_label, start_date, end_date, session_key)
 
+        save_file = filemaker.export_data3(tracks)
+
+        return send_file(save_file, as_attachment=True)
+    else:
+        return redirect(url_for('index', session_key=session_key))
 
 
 def history_data_to_driver_journal_data(history_data: TrackHistory, journal_record: JournalRecord):
@@ -106,10 +129,12 @@ def get_track_status(track_status: TrackStatus, journal_record: JournalRecord):
     status_result = track_status.label if journal_record.start_date <= track_status.changed else None
     return status_result
 
-def return_track_array(track_id, track_label, time_from, time_to):
+def return_track_array(track_id, track_label, time_from, time_to, session_key):
 
     print('Start creating client')
     navixy_client = Client()
+
+    navixy_client.hash = session_key
 
     events = []
     event = {}
